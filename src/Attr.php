42f9c353a1c4d14ae7e2ed58d4c1de31904e5c85
Gergely Laborci <gergely@laborci.hu>
2022-03-17 12:33:37 +0100

    code formatting

--- src/Attr.php
+++ src/Attr.php
@@ -1,29 +1,59 @@
 <?php namespace Atomino\Neutrons;
 
-abstract class Attr{
-	public static function get(\ReflectionClass|\ReflectionMethod|string $reflection, string|null $method = null):static|null{
-		if(is_string($reflection)) $reflection = new \ReflectionClass($reflection);
-		if(is_string($method)) $reflection = $reflection->getMethod($method);
-		$attributes = $reflection->getAttributes(static::class);
-		if(count($attributes) === 0) return null;
+abstract class Attr {
+	
+	/**
+	 * @param \ReflectionClass|\ReflectionMethod|string $reflection
+	 * @param string|null $method
+	 * @return static|null
+	 * @throws \ReflectionException
+	 */
+	public static function get(\ReflectionClass|\ReflectionMethod|string $reflection, string|null $method = null): static|null {
+		$attributes = static::getAttributes($reflection, $method);
+		if (count($attributes) === 0) return null;
 		/** @var static $instance */
-		$instance = $attributes[0]->newInstance();
-		return $instance;
+		return $attributes[0]->newInstance();
 	}
 
-	/** @return static[] */
-	public static function collect(\ReflectionClass|\ReflectionMethod|string $reflection, string|null $method = null):array{
-		if(is_string($reflection)) $reflection = new \ReflectionClass($reflection);
-		if(is_string($method)) $reflection = $reflection->getMethod($method);
-		$attributes = $reflection->getAttributes(static::class);
-		return array_map(function (\ReflectionAttribute $attribute):static{return $attribute->newInstance();}, $attributes);
+	/**
+	 * @param \ReflectionClass|\ReflectionMethod|string $reflection
+	 * @param string|null $method
+	 * @return static[]
+	 * @throws \ReflectionException
+	 */
+	public static function all(\ReflectionClass|\ReflectionMethod|string $reflection, string|null $method = null): array {
+		$attributes = static::getAttributes($reflection, $method);
+		return static::instantiateAttributes($attributes);
 	}
 
-	/** @return static[] */
-	public static function all(\ReflectionClass|\ReflectionMethod ...$reflections):array{
+	/**
+	 * @param \ReflectionClass|\ReflectionMethod ...$reflections
+	 * @return static[]
+	 */
+	public static function collect(\ReflectionClass|\ReflectionMethod ...$reflections): array {
 		/** @var static[] $attributes */
 		$attributes = [];
 		foreach ($reflections as $reflection) array_push($attributes, ... $reflection->getAttributes(static::class));
-		return array_map(function (\ReflectionAttribute $attribute):static{return $attribute->newInstance();}, $attributes);
+		return static::instantiateAttributes($attributes);
+	}
+
+	/**
+	 * @param \ReflectionClass|\ReflectionMethod|string $reflection
+	 * @param string|null $method
+	 * @return \ReflectionAttribute[]
+	 * @throws \ReflectionException
+	 */
+	private static function getAttributes(\ReflectionClass|\ReflectionMethod|string $reflection, string|null $method = null): array {
+		if (is_string($reflection)) $reflection = new \ReflectionClass($reflection);
+		if (is_string($method) && $reflection instanceof \ReflectionClass) $reflection = $reflection->getMethod($method);
+		return $reflection->getAttributes(static::class);
+	}
+
+	/**
+	 * @param \ReflectionAttribute[] $attributes
+	 * @return static[]
+	 */
+	private static function instantiateAttributes(array $attributes): array {
+		return array_map(function (\ReflectionAttribute $attribute): static { return $attribute->newInstance(); }, $attributes);
 	}
 }
